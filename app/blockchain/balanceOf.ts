0369f5a19339f2788d974d5e1acfd0d0aec17c76
Lucas Silveira <29531+lucasfernandes@users.noreply.github.com>
2025-05-07 22:18:32 -0300

    first version

---
+++ app/blockchain/balanceOf.ts
@@ -0,0 +1,29 @@
+import { Contract, formatUnits, JsonRpcProvider } from "ethers";
+const abi = [
+  {
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export async function balanceOf(userAddress: string): Promise<number> {
+  try {
+    const nodeUrl = process.env.NEXT_PUBLIC_LINEA_NODE_RPC_URL as string;
+    const USDC_ADDRESS = process.env.NEXT_PUBLIC_LINEA_USDC_ADDRESS as string;
+
+    const provider = new JsonRpcProvider(nodeUrl);
+    const contract = new Contract(USDC_ADDRESS, abi, provider);
+    const balance = Number(
+      formatUnits(await contract?.balanceOf(userAddress), 6)
+    );
+    return balance;
+  } catch (error) {
+    console.error("Error fetching balance:", error);
+    return 0;
+  }
+}
