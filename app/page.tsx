078dc7e6dbfb5fc66e88ef875bcbb4dfac6b2edf
Lucas Silveira <29531+lucasfernandes@users.noreply.github.com>
2025-05-12 18:06:58 -0300

    icons updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import Config from "./components/config";
 import Card from "./components/card";
 import { Currency, Token, TOKENS } from "./utils/contants";
 import Link from "next/link";
-import Image from "next/image";
+
+import { Cog8ToothIcon } from "@heroicons/react/24/outline";
 
 export type ConfigType = {
   account: string;
@@ -156,7 +157,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center pt-[10%] relative">
-      <main className="flex flex-col gap-6 items-center">
+      <main className="flex flex-col gap-16 items-center">
         <div className="flex flex-col items-center gap-2">
           <div className="flex justify-between w-full mb-10 text-center">
             <div className="w-max h-max">
@@ -177,26 +178,7 @@ export default function Home() {
               onClick={() => setOpen(true)}
               className="w-8 h-8 rounded-full text-white/70 shadow-md flex items-center justify-center transition-all hover:scale-105 hover:text-white hover:cursor-pointer"
             >
-              <svg
-                data-slot="icon"
-                fill="none"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
-                ></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                ></path>
-              </svg>
+              <Cog8ToothIcon className="w-8 h-8" />
             </button>
           </div>
           <Card
@@ -271,7 +253,7 @@ export default function Home() {
           in your card currency
         </div>
       </main>
-      <footer className="fixed bottom-0 w-full p-4 text-center text-white/70">
+      <footer className="fixed bottom-10 w-full p-4 text-center text-white/70">
         <div className="flex items-center justify-center gap-1 text-sm">
           <span>Quotes from</span>
           <Link
@@ -280,14 +262,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className="flex items-center justify-center gap-2 rounded-full"
           >
-            <span>Frankfuter</span>
-            <Image
-              src="/frankfurt.png"
-              alt="Frankfurter"
-              width={24}
-              height={24}
-              className="box-shadow"
-            />
+            Frankfuter
           </Link>
         </div>
       </footer>
